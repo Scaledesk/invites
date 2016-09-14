e8f1f8b05f27ee87a7d99acda2d3ebc48ce7f394
Neeraj <nkneeraj99@gmail.com>
2016-09-14 19:14:32 +0530

    Redesign For mobile view

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
             // Using jQuery's animate() method to add smooth page scroll
             // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
-                scrollTop: $(hash).offset().top - 170
+                scrollTop: $(hash).offset().top - 40
             }, 800, function(){
 
                 // Add hash (#) to URL when done scrolling (default click behavior)
@@ -55,22 +55,7 @@ $(document).ready(function(){
 
 
 
-//Service Count numbers//
 
-$('.count + i').each(function () {
-    $(this).prop('Counter',0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: 4000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-        }
-    });
-});
-
-
-//Ends here//
 
 
 
@@ -81,60 +66,12 @@ $(window).scroll(function(event){
     var yOffset = window.pageYOffset;
     var breakpoint = 550;
     if (yOffset > breakpoint){
-        $(".custom-nav").addClass('active');
+        $(".custom-nav").addClass('active ');
     }else{
-        $(".custom-nav").removeClass('active');
+        $(".custom-nav").removeClass('active ');
     }
 
 });
-
-
-//Ends here//
-
-
-
-
-
-
-//Isotopes js start here///
-
-
-// external js: isotope.pkgd.js
-
-// init Isotope
-var $grid = $('.grid').isotope({
-    itemSelector: '.element-item',
-    layoutMode: 'fitRows'
-});
-// filter functions
-var filterFns = {
-    // show if number is greater than 50
-    numberGreaterThan50: function() {
-        var number = $(this).find('.number').text();
-        return parseInt( number, 10 ) > 50;
-    },
-    // show if name ends with -ium
-    ium: function() {
-        var name = $(this).find('.name').text();
-        return name.match( /ium$/ );
-    }
-};
-// bind filter button click
-$('.filters-button-group').on( 'click', 'button', function() {
-    var filterValue = $( this ).attr('data-filter');
-    // use filterFn if matches value
-    filterValue = filterFns[ filterValue ] || filterValue;
-    $grid.isotope({ filter: filterValue });
-});
-// change is-checked class on buttons
-$('.button-group').each( function( i, buttonGroup ) {
-    var $buttonGroup = $( buttonGroup );
-    $buttonGroup.on( 'click', 'button', function() {
-        $buttonGroup.find('.is-checked').removeClass('is-checked');
-        $( this ).addClass('is-checked');
-    });
-});
-
 
 
 //Ends here//
@@ -171,6 +108,10 @@ $back_to_top.on('click', function(event){
     );
 });
 
+
+
+
+
 // you want to enable the pointer events only on click;
 
         $('#map_canvas1').addClass('scrolloff'); // set the pointer events to none on doc ready
@@ -186,3 +127,20 @@ $back_to_top.on('click', function(event){
 
 
 //Ends here//
+
+
+
+
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+        $('nav').addClass('shrink');
+    } else {
+        $('nav').removeClass('shrink');
+    }
+});
+
+
+
+
+

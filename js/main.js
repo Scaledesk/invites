7b53f7a7caef884efb57c286978be71943f8d545
sanchitrathee <sanchit2411@gmail.com>
2016-10-10 17:39:03 +0530

    Slider in Gallery Mobile view and Top section for mobile change

--- js/main.js
+++ js/main.js
@@ -171,6 +171,7 @@ $("#owl-demo").owlCarousel({
 
 $("#owl-demo1").owlCarousel({
     navigation: true,
+    pagination: false,
     navigationText: [
         "<i class='fa fa-angle-left fa-2x' ></i>",
         "<i class='fa fa-angle-right fa-2x'></i>"

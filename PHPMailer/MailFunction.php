5ac50862e8ab281dc74b713636170e67ce71381f
sanchit <sanchit@scorecard.dreamhost.com>
2016-10-21 23:36:37 -0700

    fixed mail function

--- PHPMailer/MailFunction.php
+++ PHPMailer/MailFunction.php
@@ -5,12 +5,12 @@ $email = $_POST['email'];
 
 $mail = new PHPMailer; 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'mail.inviteaffairs.com';  // Specify main and backup SMTP servers
+$mail->Host = 'sub5.mail.dreamhost.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'info@inviteaffairs.com';                 // SMTP username
 $mail->Password = 'invite@2016';                           // SMTP password
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465;  
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;  
 $mail->IsHTML(true);
 
 
@@ -18,12 +18,12 @@ $mail->IsHTML(true);
 
 $mail1 = new PHPMailer; 
 $mail1->isSMTP();                                      // Set mailer to use SMTP
-$mail1->Host = 'mail.inviteaffairs.com';  // Specify main and backup SMTP servers
+$mail1->Host = 'sub5.mail.dreamhost.com';  // Specify main and backup SMTP servers
 $mail1->SMTPAuth = true;                               // Enable SMTP authentication
 $mail1->Username = 'info@inviteaffairs.com';                 // SMTP username
 $mail1->Password = 'invite@2016';                           // SMTP password
-$mail1->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail1->Port = 465;  
+$mail1->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail1->Port = 587;  
 $mail1->IsHTML(true);
 $mail1->setFrom('info@inviteaffairs.com', 'Contact Form');
 $mail1->addAddress($email);               
@@ -42,7 +42,7 @@ $mail->isHTML(true);
 $mail->Subject = 'Contact Details';
 $mail->Body    = '<p>Name: '.$_POST['name'].'</p><p>Email: '.$email.'</p><p>Number: '.$_POST['mobile'].'</p><p>Message: '.$_POST['message'].'</p>';
 if(!$mail->send()) {
-    echo 'error';
+    echo 'error :' . $mail->ErrorInfo;
 } else {
 	header('Location: ../thankyou.html');
 }
